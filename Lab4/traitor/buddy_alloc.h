f9f2a484b70b10b8620de21c675ce6f02253a725
NightFox64 <91380334+NightFox64@users.noreply.github.com>
2024-12-25 11:16:16 +0300

    Edit headers

--- Lab4/traitor/buddy_alloc.h
+++ Lab4/traitor/buddy_alloc.h
@@ -1,13 +1,13 @@
-#ifndef OS_LAB_ALLOCATOR2_H
-#define OS_LAB_ALLOCATOR2_H
+#ifndef BUDDY_ALLOC_H
+#define BUDDY_ALLOC_H
 
 #include <stddef.h>
 
-typedef struct Allocator2 Allocator2;
+typedef struct Allocator Allocator;
 
-Allocator2* allocator2_create(size_t size);
-void allocator2_destroy(Allocator2* allocator);
-void* allocator2_alloc(Allocator2* allocator, size_t size);
-void allocator2_free(Allocator2* allocator, void* memory);
+Allocator* allocator_create(void *const memory, const size_t size);
+void allocator_destroy(Allocator* allocator);
+void* allocator_alloc(Allocator* allocator, size_t size);
+void allocator_free(Allocator* allocator, void* memory);
 
-#endif //OS_LAB_ALLOCATOR2_H
+#endif
--- Lab4/traitor/mc_karels_alloc.h
+++ Lab4/traitor/mc_karels_alloc.h
@@ -1,20 +1,12 @@
-#ifndef OS_LAB_ALLOCATOR_H
-#define OS_LAB_ALLOCATOR_H
+#ifndef MC_KARELS_ALLOC_H
+#define MC_KARELS_ALLOC_H
 
 #include <stddef.h>
 
 typedef struct Allocator Allocator;
-
-// Инициализация аллокатора
 Allocator* allocator_create(void *const memory, const size_t size);
-
-// Деинициализация аллокатора
 void allocator_destroy(Allocator *const allocator);
-
-// Выделение памяти
 void* allocator_alloc(Allocator *const allocator, const size_t size);
-
-// Освобождение памяти
 void allocator_free(Allocator *const allocator, void *const memory);
 
-#endif //OS_LAB_ALLOCATOR_H
+#endif
